8c091e3a0bc364a648ece0120272c1dd69e920d0
gurhanpro <ziyaadyare@gmail.com>
2020-12-04 10:37:10 +0200

    added controller and service for getting lists

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
+  @Get('/list')
+  getLists(): Promise<any> {
+    return this.appService.getBoardList();
+  }
 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,34 @@
 import { Injectable } from '@nestjs/common';
+const request = require("request");
 
 @Injectable()
 export class AppService {
+
+  async getBoardList(): Promise<any> {
+    try {
+      let lists: any = await this.fetchBoardListRequest();
+      lists = JSON.parse(lists);
+      return lists
+    } catch (error) {
+      console.log(error);
+      return error
+    }
+  }
+
+  fetchBoardListRequest() {
+    const { TRELLO_KEY, TRELLO_TOKEN } = process.env;
+    console.log('keys: ', TRELLO_KEY, 'token: ', TRELLO_TOKEN);
+    
+    var options = {
+      'method': 'GET',
+      'url': `https://api.trello.com/1/boards/5fc66eabd8c4e11a83a2ac82/lists?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&cards=open`,
+    };
+    return new Promise((resolve, reject) => {
+      request(options, function (error, response, body) {
+        resolve(body)
+        reject(error)
+      });
+    })
+  }
 
 }
